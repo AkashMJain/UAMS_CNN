c4196731b534bbe715f48ea3d7b18a240e8b6240
akash <akashjaincoder@icloud.com>
2019-03-16 17:14:26 +0530

    uams_demo.py working

--- utils/datasets.py
+++ utils/datasets.py
@@ -67,3 +67,17 @@ def split_data(x, y, validation_split=.2):
     train_data = (train_x, train_y)
     val_data = (val_x, val_y)
     return train_data, val_data
+
+
+def get_labels(dataset_name):
+    if dataset_name == 'fer2013':
+        return {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy',
+                4: 'sad', 5: 'surprise', 6: 'neutral'}
+    elif dataset_name == 'imdb':
+        return {0: 'woman', 1: 'man'}
+    elif dataset_name == 'KDEF':
+        return {0: 'AN', 1: 'DI', 2: 'AF', 3: 'HA', 4: 'SA', 5: 'SU', 6: 'NE'}
+    elif dataset_name == 'uams-dataset':
+        return {0: 'attentive', 1: 'un-attentive'}
+    else:
+        raise Exception('Invalid dataset name')
